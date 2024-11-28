eea27f28915743198350cb70c08f325fe8e1fa29
fvesanen <fanni.vesanen@gmail.com>
2024-11-28 15:14:33 +0200

    chaos

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // Band structure of the JSON data
@@ -17,64 +18,114 @@ type Band struct {
 	Members      []string `json:"members"`
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
-	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
+	Location     []string
+	Dates        DatesURL
+	Relation     RelationsURL
 }
 
-type Location struct {
-	ID        int      `json:"id"`
-	Locations []string `json:"locations"`
+type LocationURL struct {
+	Index []struct {
+		ID        int      `json:"id"`
+		Locations []string `json:"locations"`
+		Dates     string   `json:"dates"`
+	} `json:"index"`
 }
 
-type Date struct {
-	ID    int      `json:"id"`
-	Dates []string `json:"dates"`
+type DatesURL struct {
+	Index []struct {
+		ID    int      `json:"id"`
+		Dates []string `json:"dates"`
+	} `json:"index"`
 }
 
-type Relation struct {
-	ID        int                 `json:"id"`
-	Relations map[string][]string `json:"datesLocations"`
+type RelationsURL struct {
+	Index []struct {
+		ID             int                 `json:"id"`
+		DatesLocations map[string][]string `json:"datesLocations"`
+	} `json:"index"`
 }
 
 var tmpl = template.Must(template.ParseFiles("index.html"))
 
 func main() {
 	var artists []Band
-	var locations []Location
-	var dates []Date
-	var relations []Relation
+	var locationData LocationURL
+	var dates DatesURL
+	var relations RelationsURL
 
+	log.Println("fething artist data")
 	err := fetchData("https://groupietrackers.herokuapp.com/api/artists", &artists)
 	if err != nil {
 		log.Fatalf("Error fetching artists: %v", err)
 	}
-	err = fetchData("https://groupietrackers.herokuapp.com/api/locations", &locations)
+	log.Println("fething location data")
+	err = fetchData("https://groupietrackers.herokuapp.com/api/locations", &locationData)
 	if err != nil {
 		log.Fatalf("Error fetching artists: %v", err)
 	}
+	log.Println("fething dates data")
 	err = fetchData("https://groupietrackers.herokuapp.com/api/dates", &dates)
 	if err != nil {
 		log.Fatalf("Error fetching artists: %v", err)
 	}
+	log.Println("fething releations data")
 	err = fetchData("https://groupietrackers.herokuapp.com/api/relation", &relations)
 	if err != nil {
 		log.Fatalf("Error fetching artists: %v", err)
 	}
 
-	for _, artist := range artists {
-		fmt.Printf("Artist: %s, Creation Date: %d\n", artist.Name, artist.CreationDate)
-	}
+	// for _, artist := range artists {
+	// 	fmt.Printf("Artist: %s, Creation Date: %d\n", artist.Name, artist.CreationDate)
+	// }
+
+	addLocation(artists, locationData)
+
 	// Serving static files like CSS
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
-
+	log.Println("rendering BandPAge")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl.ExecuteTemplate(w, "index.html", artists)
+		tmpl.Execute(w, artists)
 	})
 
-	fmt.Println("Server started on http://localhost:8090")
-	http.ListenAndServe(":8090", nil)
+	fmt.Println("Server started on http://localhost:80")
+	log.Fatal(http.ListenAndServe(":80", nil))
 
+}
+
+// Reading locations from LocationURL and adding to Band struct by matching IDs
+func addLocation(artists []Band, locationData LocationURL) {
+
+	cleanLocation := func(s []string) []string {
+		var location []string
+
+		for _, word := range s {
+			modWord := ""
+			for i, char := range word {
+				if char == '_' {
+					modWord += " "
+				} else if char == '-' {
+					modWord += " - "
+				} else if i == 0 || word[i-1] == '_' || word[i-1] == '-' {
+					modWord += strings.ToUpper(string(char))
+				} else {
+					modWord += string(char)
+				}
+			}
+			location = append(location, modWord)
+		}
+		return location
+	}
+
+	for i := range artists {
+		// Find the corresponding location data for the artist based on ID
+		for _, loc := range locationData.Index {
+			if loc.ID == artists[i].ID {
+				cleanLocation := cleanLocation(loc.Locations)
+				artists[i].Location = cleanLocation
+				break
+			}
+		}
+	}
 }
 
 func fetchData(url string, target interface{}) error {
@@ -88,5 +139,9 @@ func fetchData(url string, target interface{}) error {
 	if err != nil {
 		return err
 	}
+	//fmt.Println(string(data)) // Debug the API response
 	return json.Unmarshal(data, target)
 }
+
+
+// possible changes: Reading locations into a 
